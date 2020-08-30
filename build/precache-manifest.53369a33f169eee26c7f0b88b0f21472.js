731846d2b35f3415806cd2ef5435049dfe0f7ad8
sebastian <sebastianserri@gmail.com>
2020-08-30 17:03:25 -0300

    init

---
+++ build/precache-manifest.53369a33f169eee26c7f0b88b0f21472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67436f808994bbc2cb7e99c83fc5fb6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f60739dec0972d5488fa",
+    "url": "/static/css/main.a134560c.chunk.css"
+  },
+  {
+    "revision": "766b5190b3f795b274de",
+    "url": "/static/js/2.f64b6022.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f64b6022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f60739dec0972d5488fa",
+    "url": "/static/js/main.fdd3d620.chunk.js"
+  },
+  {
+    "revision": "a6e86a5e31dac150a57e",
+    "url": "/static/js/runtime-main.6464ed0c.js"
+  }
+]);
